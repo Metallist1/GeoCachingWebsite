9d11db0f759d3b757c76cd8d049364a55c02fa42
Nedas Surkus <nedassurkus@gmail.com>
2021-08-25 02:26:40 +0200

    Changed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   firebaseConfig: {
     apiKey: "AIzaSyA0cY_UeQkk8r2TJph5NtEerc9_u3m48m8",
     authDomain: "geocaching-b04b9.firebaseapp.com",
+    databaseURL: "https://geocaching-b04b9-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "geocaching-b04b9",
     storageBucket: "geocaching-b04b9.appspot.com",
     messagingSenderId: "743096754501",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,11 @@ export const environment = {
   firebaseConfig: {
     apiKey: "AIzaSyA0cY_UeQkk8r2TJph5NtEerc9_u3m48m8",
     authDomain: "geocaching-b04b9.firebaseapp.com",
+    databaseURL: "https://geocaching-b04b9-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "geocaching-b04b9",
     storageBucket: "geocaching-b04b9.appspot.com",
     messagingSenderId: "743096754501",
-    appId: "1:743096754501:web:cb1f936fdf1d10ab205e2c",
-    databaseURL: "https://geocaching-b04b9-default-rtdb.europe-west1.firebasedatabase.app/"
+    appId: "1:743096754501:web:cb1f936fdf1d10ab205e2c"
   },
 };
 
